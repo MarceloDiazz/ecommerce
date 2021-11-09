abc5869836e22a3632ed81ceb8a2e64c907393ae
MateoFMoreno <mateo.moreno999@gmail.com>
2021-11-09 15:43:33 -0300

    rutas setup inicial

--- api/index.js
+++ api/index.js
@@ -1,11 +1,34 @@
-const db = require('./config/db');
-const express = require('express');
+const express = require("express");
+const session = require("express-session");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const volleyball = require("volleyball");
+const passport = require("passport");
+
+const routes = require("./routes");
+const db = require("./config/db");
 const app = express();
-const port = 3001   
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // pasar a falso antes de terminar
+app.use(cors());
+app.use(cookieParser());
+app.use(volleyball);
 
+app.use(session({ secret: process.env.SESSION_KEY, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use("/api", routes);
 
-db.then(() => {app.listen(port, () => {
-    console.log(`Server on ${port}`);
-})});
+app.get("/", (req, res) => {
+    res.send("anda");
+});
+
+const port = 3001;
+
+db.then(() => {
+    app.listen(port, () => {
+        console.log(`Server on ${port}`);
+    });
+});
